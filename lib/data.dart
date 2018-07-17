3005f547369266327594bb519ae698ca9560571b
hiroshi.matsui <hiroshi.matsui@STTA18006-Hiroshi-Matsui.local>
2018-07-17 16:42:46 +0900

    Initial Commit

---
+++ lib/data.dart
@@ -0,0 +1,11 @@
+class Item {
+  String image, category, color;
+
+  Item(this.image, this.category, this.color);
+}
+
+final List<Item> items = [
+  Item('./images/T_big_white.PNG','T-shirt','white'),
+  Item('./images/slucks_black.PNG','slucks','black'),
+  Item('./images/dress_shoes_black_martens.PNG','dress shoes','black'),
+];
---
+++ lib/home.dart
@@ -0,0 +1,60 @@
+import 'package:closet_app/data.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final appBar = AppBar(
+      elevation: .5,
+      leading: IconButton(
+        icon: Icon(Icons.menu),
+        onPressed: (){},
+      ),
+      title: Text("All Items"),
+      actions: <Widget>[
+        IconButton(
+          icon: Icon(Icons.search),
+          onPressed: (){},
+        )
+      ],
+    );
+
+    createTile(Item item) => Hero(
+        tag: item.category,
+        child: Material(
+            elevation: 15.0,
+            shadowColor: Colors.white,
+            child: InkWell(
+                onTap: (){},
+                child: Image(
+                  image: AssetImage(item.image),
+                  fit: BoxFit.cover,
+                )
+            )
+        )
+    );
+    
+    final grid = CustomScrollView(
+      primary: false,
+      slivers: <Widget>[
+        SliverPadding(
+          padding: EdgeInsets.all(16.0),
+          sliver: SliverGrid.count(
+            childAspectRatio: 1.0,
+            crossAxisCount: 4,
+            mainAxisSpacing: 0.0,
+            crossAxisSpacing: 0.0,
+            children: items.map((item) => createTile(item)).toList(),
+          ),
+        )
+      ],
+    );
+
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      appBar: appBar,
+      body: grid,
+    );
+  }
+
+}
