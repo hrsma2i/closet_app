1597747b36d79717393aa87db965ece29dfcb4d1
hiroshi.matsui <hiroshi.matsui@STTA18006-Hiroshi-Matsui.local>
2018-07-18 19:46:07 +0900

    Make item/outfit list

--- lib/data.dart
+++ lib/data.dart
@@ -1,11 +1,58 @@
-class Item {
-  String image, category, color;
+import 'package:flutter/material.dart';
+import 'dart:math';
 
-  Item(this.image, this.category, this.color);
+class Item {
+  String imageFile, category, color;
+
+  Item(this.imageFile, this.category, this.color);
 }
 
-final List<Item> items = [
-  Item('./images/T_big_white.PNG','T-shirt','white'),
-  Item('./images/slucks_black.PNG','slucks','black'),
-  Item('./images/dress_shoes_black_martens.PNG','dress shoes','black'),
+class Outfit {
+  List<Item> items;
+
+  Outfit(this.items);
+}
+
+
+List<Outfit> outfits = [
+  Outfit([
+    Item('./images/T_big_white.PNG',
+         'T-shirt','white'),
+    Item('./images/slucks_black.PNG',
+         'slucks','black'),
+    Item('./images/dress_shoes_black_martens.PNG',
+         'dress shoes','black'),
+  ]),
+  Outfit([
+    Item('./images/shirtshalf_aloha.PNG',
+        'half sleeve shirts','grey'),
+    Item('./images/denim_indigo.PNG',
+        'denim pants','blue'),
+    Item('./images/dress_shoes_black_martens.PNG',
+        'dress shoes','black'),
+    Item('./images/dress_shoes_black_martens.PNG',
+        'dress shoes','black'),
+    Item('./images/dress_shoes_black_martens.PNG',
+        'dress shoes','black'),
+  ]),
 ];
+
+List<Item> items = outfits.expand(
+  (outfit) => outfit.items
+).toList();
+
+List<Widget> itemImages = items.map(
+  (item) => Image(image: AssetImage(item.imageFile)),
+).toList();
+
+List<Widget> outfitImages = outfits.map(
+  (outfit) => GridView.count(
+    primary: false,
+    padding: const EdgeInsets.all(0.0),
+    crossAxisSpacing: 10.0,
+    crossAxisCount: sqrt(outfit.items.length).ceil(),
+    children: outfit.items.map(
+      (item) => Image(image: AssetImage(item.imageFile))
+    ).toList(),
+  )
+).toList();
--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,177 @@
+import 'dart:io';
+import 'dart:async';
+import 'package:path_provider/path_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'data.dart';
-import 'home.dart';
-import 'detail.dart';
+import 'package:sqflite/sqflite.dart';
 
 void main() => runApp(new MyApp());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Closet App',
-      debugShowCheckedModeBanner: false,
-      theme: new ThemeData.dark(),
-      home: Home(),
-      onGenerateRoute: (settings) => generateRoute(settings),
-    );
+//class MyApp extends StatelessWidget {
+//  // This widget is the root of your application.
+//  @override
+//  Widget build(BuildContext context) {
+//    return new MaterialApp(
+//      title: 'Closet App',
+//      debugShowCheckedModeBanner: false,
+//      theme: new ThemeData.dark(),
+//      home: Home(),
+//      onGenerateRoute: (settings) => generateRoute(settings),
+//    );
+//  }
+//
+//  generateRoute(RouteSettings settings) {
+//    final path = settings.name.split('/');
+//    final category = path[1];
+//
+//    Item item = items.firstWhere((it) => it.category == category);
+//    return MaterialPageRoute(
+//      settings: settings,
+//      builder: (context) => Detail(item),
+//    );
+//  }
+//}
+
+class MyApp extends StatefulWidget {
+  MyAppState createState() => new MyAppState();
+}
+
+class MyAppState extends State<MyApp> {
+
+  int index = 0;
+
+  Future getImage() async {
+    var imageFile = await ImagePicker.pickImage(source: ImageSource.gallery);
+    setState(() {
+      itemImages.add(Image.file(imageFile));
+    });
   }
 
-  generateRoute(RouteSettings settings) {
-    final path = settings.name.split('/');
-    final category = path[1];
 
-    Item item = items.firstWhere((it) => it.category == category);
-    return MaterialPageRoute(
-      settings: settings,
-      builder: (context) => Detail(item),
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: new Scaffold(
+        body: new Stack(
+          children: [
+            Offstage(
+              offstage: index != 0,
+              child: new TickerMode(
+                  enabled: index == 0,
+                  child: PageWithTabs(
+                    'Outfits',
+                    [
+                      TabItem("today's",  outfitImages),
+                      TabItem("possible", outfitImages),
+                      TabItem("all",      outfitImages),
+                    ],
+                  ),
+              ),
+            ),
+            Offstage(
+              offstage: index != 1,
+              child: new TickerMode(
+                enabled: index == 1,
+                child: PageWithTabs(
+                  'Items',
+                  [
+                    TabItem("all", itemImages),
+                    TabItem("owned", itemImages),
+                    TabItem("to buy", itemImages),
+                  ],
+                ),
+              ),
+            ),
+            Offstage(
+              offstage: index != 2,
+              child: new TickerMode(
+                enabled: index == 2,
+                child: PageWithTabs(
+                  'Find',
+                  [
+                    TabItem("outfit", outfitImages),
+                    TabItem("new item", itemImages),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+        floatingActionButton: new FloatingActionButton(
+          onPressed: getImage,
+          tooltip: 'Pick Image',
+          child: new Icon(Icons.add_a_photo),
+        ),
+        bottomNavigationBar: new BottomNavigationBar(
+          currentIndex: index,
+          onTap: (int index) { setState((){ this.index = index; }); },
+          items: <BottomNavigationBarItem>[
+            new BottomNavigationBarItem(
+              icon: new ImageIcon(
+                AssetImage('./icons/outfit_fill.png'),
+              ),
+              title: new Text("outfits"),
+            ),
+            new BottomNavigationBarItem(
+              icon: new ImageIcon(
+                AssetImage('./icons/T_fill.png'),
+              ),
+              title: new Text("items"),
+            ),
+            new BottomNavigationBarItem(
+              icon: Icon(Icons.search),
+              title: new Text("find"),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
+
+class TabItem extends StatelessWidget {
+  String tabName;
+  List<Widget> grids;
+
+  TabItem(this.tabName, this.grids);
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.count(
+      primary: false,
+      padding: const EdgeInsets.all(20.0),
+      crossAxisSpacing: 10.0,
+      crossAxisCount: 3,
+      children: grids,
+    );
+  }
+}
+
+class PageWithTabs extends StatelessWidget {
+  String pageName;
+  List<TabItem> tabItems;
+
+  PageWithTabs(this.pageName, this.tabItems);
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: tabItems.length,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Outfits"),
+          bottom: TabBar(
+            tabs: tabItems.map(
+                (tabItem) => new Tab(text: tabItem.tabName)
+            ).toList(),
+          ),
+        ),
+        body: TabBarView(
+          children: tabItems,
+        ),
+      ),
+    );
+  }
+}
+
